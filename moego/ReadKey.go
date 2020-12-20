dbfef84e0f809859cb71111bb698f11fd138435d
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-12-20 13:16:45 +0900

    Update readkey.go

--- moego/ReadKey.go
+++ moego/ReadKey.go
@@ -2,14 +2,16 @@ package moego
 
 import (
     "syscall"
+    "unicode/utf8"
 )
 
-func (e *Editor) {
+func (e *Editor) ReadKey() {
     buf := make([]byte, 64)
 
     // 無限ループ
     for {
         if n, err := syscall.Read(0, buf); err == nil {
+            b := buf[n:]
             for {
                 r, n := e.parseKey(b)
 
@@ -18,8 +20,30 @@ func (e *Editor) {
                 }
 
                 e.keyChan <- r
-                b = b[n:]
+                b= b[n:]
             }
         }
     }
+}
+
+func (e *Editor) parseKey(buf []byte) (rune, int) {
+    // parse escape sequence
+    if len(buf) == 3 {
+        if buf[0] == byte(27) && buf[1] == '[' {
+            switch buf[2] {
+            case 'A':
+                return ArrowUp, 3
+            case 'B':
+                return ArrowDown, 3
+            case 'C':
+                return ArrowRight, 3
+            case 'D':
+                return ArrowLeft, 3
+             default:
+                 return DummyKey, 0
+            }
+        }
+    }
+    // parse bytes as utf-8
+    return utf8.DecodeRune(buf)
 }
